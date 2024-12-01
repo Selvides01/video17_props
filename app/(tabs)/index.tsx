6ff539b6056d2cff4857301f5e19faf23b04c32d
Selvides01 <selvides16266@gmail.com>
2024-12-01 12:12:36 +0700

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,55 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+class MateriFlexBox extends Component {
+  render() {
+    return (
+      <View style={{ padding: 10 }}>
+        {/* Flexbox untuk kotak warna-warni */}
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#c8d6e5',
+            alignItems: 'center', 
+            justifyContent: 'flex-end',
+          }}
+        >
+          <View style={{ backgroundColor: '#ee5253', width: 50, height: 100 }} />
+          <View style={{ backgroundColor: '#feca57', width: 50, height: 100 }} />
+          <View style={{ backgroundColor: '#1dd1a1', width: 50, height: 100 }} />
+          <View style={{ backgroundColor: '#5f27cd', width: 50, height: 100 }} />
+        </View>
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+        {/* Menu teks */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+          <Text>Beranda</Text>
+          <Text>Video</Text> 
+          <Text>Playlist</Text>
+          <Text>Komunitas</Text>
+          <Text>Channel</Text> 
+          <Text>Tentang</Text>
+        </View>
+
+
+        <View style={{  alignItems: 'center', justifyContent: 'flex-start', marginTop: 20 }}>
+          <Image
+            source={{
+              uri: 'https://i.pinimg.com/736x/51/ba/fa/51bafae3ff3c7d175d9efad4cf2a3da2.jpg',
+            }}
+            style={{
+              width: 100,
+              height: 100,
+              borderRadius: 50,
+            }}
+          />
+          <View>
+            <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'blue' }}>Selvi Desti Riyani</Text> 
+            <Text>3 JT subscriber</Text> 
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default MateriFlexBox;
